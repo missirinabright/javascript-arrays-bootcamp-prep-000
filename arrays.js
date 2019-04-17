688a19804d3be6ac7345b98f1b80f398702c2a43
Irina Bright <missirinabright@gmail.com>
2019-04-17 22:49:06 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,2 +1,14 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
+function addElementToBeginningOfArray(chocolateBars, candyString){
+  return [candyString, ...chocolateBars];
+}
+ 
+function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString){
+  chocolateBars.unshift(candyString);
+  return chocolateBars;
+}
+ 
+function addElementToEndOfArray(chocolateBars, candyString){
+  return [...chocolateBars, candyString];
+}
