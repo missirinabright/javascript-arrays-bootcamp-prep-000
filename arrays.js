07b5fe301c3e0e150cb17e696a078a86ad845216
Irina Bright <missirinabright@gmail.com>
2019-04-17 22:47:13 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,11 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+var addElementToBeginningOfArray = new Array();
+addElementToBeginningOfArray = ['array', 'element'];
+  addElementToBeginningOfArray.unshift();
+  
+  /* describe('addElementToBeginningOfArray(array, element)', () => {
+    it('adds an element to the beginning of an array', () => {
+      expect(addElementToBeginningOfArray([1], 'foo')).to.eql(['foo', 1])
+    })
+ */
